d6ac513c9a48d6bddc82cd7776d53d281fe16dbb
Lucas Almeida <124281689+LucaAlmeidaDev@users.noreply.github.com>
2024-02-06 15:34:22 -0300

    Final Commit

--- script.js
+++ script.js
@@ -1,6 +1,57 @@
+let display = document.getElementById('display')
+let memory = '';
+let operator = '';
+
+
 function NumDisplay(value) {
-    document.getElementById('display').value += value;
+    display.value += value;
 }
+
 function Remove() {
-    document.getElementById('display').value = '';
+    display.value = '';
+}
+
+function SetOperator(op) {
+    if (display.value !== '') {
+        memory = display.value;
+        operator = op;
+        display.value = memory + ' ' + operator + ' ';
+    }
+}
+
+function PerformOperation() {
+    if (memory !== '' && display.value !== '') {
+        let parts = display.value.split(' ');
+        let firstOperand = parseFloat(memory);
+        let secondOperand = parseFloat(parts[2]);
+        let operation = parts[1];
+
+        switch (operation) {
+            case '+':
+                display.value = firstOperand + secondOperand;
+                break;
+            case '-':
+                display.value = firstOperand - secondOperand;
+                break;
+            case '*':
+                display.value = firstOperand * secondOperand;
+                break;
+            case '%':
+                display.value = firstOperand * (secondOperand / 100);
+                break;
+            case '/':
+                if (secondOperand !== 0) {
+                    display.value = firstOperand / secondOperand;
+                } else {
+                    display.value = 'Error';
+                }
+                break;
+            default:
+                display.value = 'Error';
+                break;
+        }
+
+        memory = '';
+        operator = '';
+    }
 }
